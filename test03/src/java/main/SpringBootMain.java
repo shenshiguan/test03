55cf8d6840f1d8cf0fee61bb7bd7c1e9deb98247
shenshiguan <1097000406@qq.com>
2018-08-07 11:12:56 +0800

    maven warning

---
+++ test03/src/java/main/SpringBootMain.java
@@ -0,0 +1,16 @@
+package java.main;
+
+public class SpringBootMain {
+
+	
+	public static void main(String[] args) {
+		System.out.println("           ....              ");
+		System.out.println("          ......             ");
+		System.out.println("         ........            ");
+		System.out.println("        ..........           ");
+		System.out.println("       ............          ");
+		System.out.println("            ..               ");
+		System.out.println("            ..               ");
+		System.out.println("            ..               ");
+	}
+}
